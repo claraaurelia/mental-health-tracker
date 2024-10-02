223f5af17e1d4cabbecbfe6840426166abd3ffd7
claraaurelia <clarasetiady@gmail.com>
2024-10-02 14:29:39 +0700

    tambahin spasi

--- main/forms.py
+++ main/forms.py
@@ -5,6 +5,7 @@ from django.utils.html import strip_tags
 class MoodEntryForm(ModelForm):
     class Meta:
         model = MoodEntry
+        
         fields = ["mood", "feelings", "mood_intensity"]
     def clean_mood(self):
         mood = self.cleaned_data["mood"]
